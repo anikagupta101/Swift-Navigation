3a019a7263d053503cfc3b751f42d7e94b5ecf40
Anika Gupta <anikagupta101@gmail.com>
2025-07-24 09:28:49 -0700

    Initial Commit

---
+++ Swift Navigation/Swift_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NavigationApp.swift
+//  Swift Navigation
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
